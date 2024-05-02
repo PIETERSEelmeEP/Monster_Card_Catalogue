e530d6225aefa9a05831268372ada8b6d6c7134a
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-05-03 11:26:28 +1200

    Create 01_Store_Existing_Monster_Cards_v3.py

---
+++ 01_Store_Existing_Monster_Cards_v3.py
@@ -0,0 +1,75 @@
+"""Third Trial of storing the existing Monster Cards. This version will be
+using dictionaries within nested dictionaries instead of using dictionaries
+within a list.
+"""
+
+# Monster Card Catalogue using dictionaries within nested dictionaries
+monster_cards_catalogue = {
+    "Stoneling": {
+        "Strength": 7,
+        "Speed": 1,
+        "Stealth": 25,
+        "Cunning": 15
+    },
+    "Vexscream": {
+        "Strength": 1,
+        "Speed": 6,
+        "Stealth": 21,
+        "Cunning": 19
+    },
+    "Dawnmirage": {
+        "Strength": 5,
+        "Speed": 15,
+        "Stealth": 18,
+        "Cunning": 22
+    },
+    "Blazegolem": {
+        "Strength": 15,
+        "Speed": 20,
+        "Stealth": 23,
+        "Cunning": 6
+    },
+    "Websnake": {
+        "Strength": 7,
+        "Speed": 15,
+        "Stealth": 10,
+        "Cunning": 5
+    },
+    "Moldvine": {
+        "Strength": 21,
+        "Speed": 18,
+        "Stealth": 14,
+        "Cunning": 5
+    },
+    "Vortexwing": {
+        "Strength": 19,
+        "Speed": 13,
+        "Stealth": 19,
+        "Cunning": 2
+    },
+    "Rotthing": {
+        "Strength": 16,
+        "Speed": 7,
+        "Stealth": 4,
+        "Cunning": 12
+    },
+    "Froststep": {
+        "Strength": 14,
+        "Speed": 14,
+        "Stealth": 17,
+        "Cunning": 4
+    },
+    "Wispghoul": {
+        "Strength": 17,
+        "Speed": 19,
+        "Stealth": 3,
+        "Cunning": 4
+    }
+}
+
+# Print the Monster Card Catalogue (In format)
+for monster_card_name, monster_card_values in monster_cards_catalogue.items():
+    print(f"\nMonster Card: {monster_card_name}")
+
+    for key in monster_card_values:
+        print(f"{key}: {monster_card_values[key]}")
