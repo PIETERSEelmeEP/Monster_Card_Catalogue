da10f2cedd61f56aa0da7b47a8be71cf07e383ae
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-05-12 19:57:26 +1200

    Update 03_Edit_Monster_Card_v1.py

--- 03_Edit_Monster_Card_v1.py
+++ 03_Edit_Monster_Card_v1.py
@@ -1,3 +1,5 @@
+
+# Asking if they want to edit the card
 edit = input("\nDo you want to edit the card? (yes or no)").lower()
 while edit == 'yes':
 
@@ -7,31 +9,26 @@ while edit == 'yes':
 
     # Allowing them to edit strength value
     if to_edit == 'strength':
-        new_strength = int(input("\nEnter monster strength (1-25): "))
-        monster_cards_catalogue[card_name]['Strength'] = \
-            value_checker(new_strength)
+        new_strength = input("\nEnter monster strength (1-25): ")
 
     # Allowing them to edit speed value
     elif to_edit == 'speed':
-        new_speed = int(input("\nEnter monster speed (1-25): "))
-        monster_cards_catalogue[card_name]['Speed'] = \
-            value_checker(new_speed)
+        new_speed = input("\nEnter monster speed (1-25): ")
 
     # Allowing them to edit stealth value
     elif to_edit == 'stealth':
-        new_stealth = int(input("\nEnter monster stealth (1-25): "))
-        monster_cards_catalogue[card_name]['Stealth'] = \
-            value_checker(new_stealth)
+        new_stealth = input("\nEnter monster stealth (1-25): ")
 
     # Allowing them to edit cunning value
     elif to_edit == 'cunning':
-        new_cunning = int(input("\nEnter monster cunning (1-25): "))
-        monster_cards_catalogue[card_name]['Cunning'] = \
-            value_checker(new_cunning)
+        new_cunning = input("\nEnter monster cunning (1-25): ")
 
     # Allowing them to save their edits
     elif to_edit == 'save':
-        print("Edits saved!")
+        monster_cards_catalogue[card_name]['new_strength'] = strength
+        monster_cards_catalogue[card_name]['new_speed'] = speed
+        monster_cards_catalogue[card_name]['new_stealth'] = stealth
+        monster_cards_catalogue[card_name]['new_cunning'] = cunning
         break
 
     # Allowing them to cancel/delete their edits
