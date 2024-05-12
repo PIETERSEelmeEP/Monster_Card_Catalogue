fb76b6908317521334d0ae2702b0815a01125e41
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-05-12 19:57:23 +1200

    Update 02_Add_Monster_Card_v3.py

--- 02_Add_Monster_Card_v3.py
+++ 02_Add_Monster_Card_v3.py
@@ -1,5 +1,7 @@
 """This program is the same as 02_Add_Monster_Card_v2, however, instead of
-using the python console, this program uses easygui
+using the python console, this program uses easygui. Due to easygui containing
+upper and lower bounds, there is no purpose for the value checker function,
+so we can remove it
 """
 
 import easygui
@@ -69,88 +71,37 @@ monster_cards_catalogue = {
 }
 
 
-# Check value is within boundaries
-def value_checker(value):
-    if not (1 <= value <= 25):
-        easygui.msgbox("Please enter a value between 1 and 25",
-                       title="Invalid Value")
-        value = easygui.integerbox("Enter value (1-25)", title="Correct Value",
-                                   lowerbound=1, upperbound=25)
-    return value
-
-
 # Create a new monster card function
 def create_new_monster_card():
     # Create a dictionary for the new Monster Card
     # Asking for the Monster Cards name
-    card_name = input("\nEnter the name of the new Monster Card: ")
+    card_name = easygui.enterbox("Enter the name of the new Monster Card: ",
+                                 title="Monster Card Name")
     monster_cards_catalogue[card_name] = {}
 
     # Asking for the strength value
-    strength = int(input("\nEnter monster strength (1-25): "))
-    strength = value_checker(strength)
+    strength = easygui.integerbox("Enter monster strength (1-25)",
+                                  title="Strength Value", lowerbound=1,
+                                  upperbound=25)
     monster_cards_catalogue[card_name]['Strength'] = strength
 
     # Asking for the speed value
-    speed = int(input("\nEnter monster speed (1-25): "))
-    speed = value_checker(speed)
+    speed = easygui.integerbox("Enter monster speed (1-25)",
+                               title="Speed Value", lowerbound=1,
+                               upperbound=25)
     monster_cards_catalogue[card_name]['Speed'] = speed
 
     # Asking for the stealth value
-    stealth = int(input("\nEnter monster stealth (1-25): "))
-    stealth = value_checker(stealth)
+    stealth = easygui.integerbox("Enter monster stealth (1-25)",
+                                 title="Stealth Value", lowerbound=1,
+                                 upperbound=25)
     monster_cards_catalogue[card_name]['Stealth'] = stealth
 
     # Asking for the cunning value
-    cunning = int(input("\nEnter monster cunning (1-25): "))
-    cunning = value_checker(cunning)
-    monster_cards_catalogue[card_name]['Cunning'] = cunning
-
-    # Asking if they want to edit the card
-    edit = input("\nDo you want to edit the card? (yes or no)").lower()
-    while edit == 'yes':
-
-        # Asking the user what part of the monster card they want to edit
-        to_edit = input("What field do you want to edit?: (Strength,"
-                        " Speed, Stealth, Cunning, Save or Cancel) ").lower()
-
-        # Allowing them to edit strength value
-        if to_edit == 'strength':
-            new_strength = int(input("\nEnter monster strength (1-25): "))
-            monster_cards_catalogue[card_name]['Strength'] = \
-                value_checker(new_strength)
-
-        # Allowing them to edit speed value
-        elif to_edit == 'speed':
-            new_speed = int(input("\nEnter monster speed (1-25): "))
-            monster_cards_catalogue[card_name]['Speed'] = \
-                value_checker(new_speed)
-
-        # Allowing them to edit stealth value
-        elif to_edit == 'stealth':
-            new_stealth = int(input("\nEnter monster stealth (1-25): "))
-            monster_cards_catalogue[card_name]['Stealth'] = \
-                value_checker(new_stealth)
-
-        # Allowing them to edit cunning value
-        elif to_edit == 'cunning':
-            new_cunning = int(input("\nEnter monster cunning (1-25): "))
-            monster_cards_catalogue[card_name]['Cunning'] = \
-                value_checker(new_cunning)
-
-        # Allowing them to save their edits
-        elif to_edit == 'save':
-            print("Edits saved!")
-            break
-
-        # Allowing them to cancel/delete their edits
-        else:
-            confirm = input("\nAre you sure you want to cancel? (yes or no) ")\
-                .lower()
-            if confirm == 'yes':
-                break
-            else:
-                continue
+    cunning = easygui.integerbox("Enter monster cunning (1-25)",
+                                 title="Cunning Value", lowerbound=1,
+                                 upperbound=25)
+    monster_cards_cat2alogue[card_name]['Cunning'] = cunning
 
 
 # Main Routine
