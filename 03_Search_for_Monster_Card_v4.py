62133510fc5b62578628682f7e722634b92b83d0
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-05-16 14:52:55 +1200

    Create 03_Search_for_Monster_Card_v4.py

---
+++ 03_Search_for_Monster_Card_v4.py
@@ -0,0 +1,87 @@
+"""In this version the program will display all the monster cards (in a list)
+and the user can select the monster card they are searching for (Using easygui)
+"""
+
+import easygui
+
+# Monster Card Catalogue using dictionaries within nested dictionaries
+monster_cards_catalogue = {
+    "Stoneling": {
+        "Strength": 7,
+        "Speed": 1,
+        "Stealth": 25,
+        "Cunning": 15
+    },
+    "Vexscream": {
+        "Strength": 1,
+        "Speed": 6,
+        "Stealth": 21,
+        "Cunning": 19
+    },
+    "Dawnmirage": {
+        "Strength": 5,
+        "Speed": 15,
+        "Stealth": 18,
+        "Cunning": 22
+    },
+    "Blazegolem": {
+        "Strength": 15,
+        "Speed": 20,
+        "Stealth": 23,
+        "Cunning": 6
+    },
+    "Websnake": {
+        "Strength": 7,
+        "Speed": 15,
+        "Stealth": 10,
+        "Cunning": 5
+    },
+    "Moldvine": {
+        "Strength": 21,
+        "Speed": 18,
+        "Stealth": 14,
+        "Cunning": 5
+    },
+    "Vortexwing": {
+        "Strength": 19,
+        "Speed": 13,
+        "Stealth": 19,
+        "Cunning": 2
+    },
+    "Rotthing": {
+        "Strength": 16,
+        "Speed": 7,
+        "Stealth": 4,
+        "Cunning": 12
+    },
+    "Froststep": {
+        "Strength": 14,
+        "Speed": 14,
+        "Stealth": 17,
+        "Cunning": 4
+    },
+    "Wispghoul": {
+        "Strength": 17,
+        "Speed": 19,
+        "Stealth": 3,
+        "Cunning": 4
+    }
+}
+
+# Main Routine
+catalogue = list(monster_cards_catalogue)
+# User selects monster card they are searching for
+card_search = easygui.choicebox("Select the Monster Card you are searching for"
+                                , title="Searching",
+                                choices=catalogue)
+
+# Print the Results
+search_results = ""
+if card_search:
+    search_results += f"Monster Name: {card_search}\n"
+    for category, values in monster_cards_catalogue[card_search].items():
+        search_results += f"{category}: {values}\n"
+    easygui.msgbox(search_results, title="Results")
+else:
+    easygui.msgbox("No card selected!", title="Results")
+
